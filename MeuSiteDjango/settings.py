8fd6d041e8ed4fb1a24a5cb1c4ae765e6d1df160
wesleynp1 <wesleynp987@gmail.com>
2026-06-04 18:02:17 -0300

    debug_toolbar removed

--- MeuSiteDjango/settings.py
+++ MeuSiteDjango/settings.py
@@ -142,10 +142,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {
     "staticfiles": {
--- MeuSiteDjango/urls.py
+++ MeuSiteDjango/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
 from . import views
 
 urlpatterns = [
@@ -27,4 +26,4 @@ urlpatterns = [
     path('logout/', views.Sair, name="logout"),
     path("vendas/",include("vendas.urls")),
     path("clientes/", include("clientes.urls")),
-]+debug_toolbar_urls()
+]
