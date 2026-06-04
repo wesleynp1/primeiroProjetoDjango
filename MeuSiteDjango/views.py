8a456decc8b42c9a05ee5daed655ba0c0f9e1a00
wesleynp1 <wesleynp987@gmail.com>
2026-06-04 13:52:14 -0300

    Logout implementado e alterações no front-end.

--- MeuSiteDjango/views.py
+++ MeuSiteDjango/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate,login
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
+from django.contrib import auth
 
 def index(request):
     return HttpResponse(_vira_html('Vá para <a href="loja">loja</a> ou <a href="vendas">vendas</a>'))
@@ -19,11 +19,11 @@ def _vira_html(mensagem : str):
     </html>
     '''
 
-def Logar(request):
+def Autenticar(request):
     if request.method == 'POST':
-        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
         if user is not None:
-            login(request, user)
+            auth.login(request, user)
             return redirect('index')
         else:
             return render(request, 'login.html',{"erro": "Login falhou"})
@@ -32,3 +32,10 @@ def Logar(request):
             return render(request, 'login.html')
         else:
             return redirect("index")
+
+def Sair(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('index')
+    else:
+        return render(request, 'logout.html')
--- vendas/views.py
+++ vendas/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import Venda
 from loja.models import Produto
@@ -6,25 +7,30 @@ from clientes.models import Cliente
 def index(request):
     return render(request,"vendas/index.html",{"vendas" : Venda.objects.all()})
 
+@login_required
 def add_form(request):
     produtos = Produto.objects.all()
     clientes = Cliente.objects.all()
     return  render(request,"vendas/add.html",{"produtos" : produtos, "clientes" : clientes})
 
+@login_required
 def add(request):
     produto = Produto.objects.get(id=request.POST["produto"])
     cliente = Cliente.objects.get(cpf=request.POST["cliente"])
     Venda.objects.create(produto=produto, cliente=cliente, quantidade=request.POST["quantidade"])
     return redirect("vendas:index")
 
+@login_required
 def delete_form(request, id):
     venda = Venda.objects.get(id=id)
     return render(request,"vendas/delete.html",{"venda" : venda})
 
+@login_required
 def delete(request, id):
     Venda.objects.get(id=id).delete()
     return redirect("vendas:index")
 
+@login_required
 def edit_form(request, id):
     venda = Venda.objects.get(id=id)
     produtos = Produto.objects.all()
@@ -32,6 +38,7 @@ def edit_form(request, id):
 
     return render(request,"vendas/edit.html",{"venda" : venda,"produtos" : produtos, "clientes" : clientes})
 
+@login_required
 def edit(request, id):
     venda = Venda.objects.get(id=id)
     venda.quantidade = request.POST["quantidade"]
